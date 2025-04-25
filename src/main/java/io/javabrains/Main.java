ca82592bd68bf5d9670b72233828f6a651ffabbf
Joseph McHugh <joseph.mchugh@volvo.com>
2025-04-25 10:26:32 -0400

    Commit

---
+++ src/main/java/io/javabrains/Main.java
@@ -0,0 +1,12 @@
+package io.javabrains;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Employee employee = new Employee();
+    employee.setId(1);
+    employee.setName("Foo Bar");
+
+
+    }
+}
